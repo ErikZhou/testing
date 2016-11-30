72e5e34fe95ea6e3f0ca08f7fb48d869bd7240de
ErikZhou <qqzhoucn@gmail.com>
2016-11-30 22:08:44 +0800

    Update node.js

--- node.js
+++ node.js
@@ -8,3 +8,15 @@ http://stackoverflow.com/questions/23006804/getting-error-error-cannot-find-modu
 Cannot find module 'socket.io' windows 10
 npm install --save socket.io
 The --save flag will add the given dependency to your application's package.json for easier installation in the future
+
+
+down vote
+accepted
+Simple solution (if you are not interested in coming back to the process, just want it to keep running):
+
+nohup node server.js &
+Powerful solution (allows you to reconnect to the process if it is interactive):
+
+screen
+
+http://stackoverflow.com/questions/4797050/how-to-run-process-as-background-and-never-die
