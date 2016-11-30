e7942fc840c8f36145006f3ededa61b2106ea6e1
ErikZhou <qqzhoucn@gmail.com>
2016-11-30 13:46:10 +0800

    Update node.js

--- node.js
+++ node.js
@@ -2,4 +2,5 @@ nodejs
 
 Getting error, Error: Cannot find module ‘express’ after npm install
 npm link express
+
 http://stackoverflow.com/questions/23006804/getting-error-error-cannot-find-module-express-after-npm-install
