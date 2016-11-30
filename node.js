9b45da51f4c59323533bdbdeb6c34ad116354873
ErikZhou <qqzhoucn@gmail.com>
2016-11-30 14:17:50 +0800

    Update node.js

--- node.js
+++ node.js
@@ -4,3 +4,7 @@ Getting error, Error: Cannot find module ‘express’ after npm install
 npm link express
 
 http://stackoverflow.com/questions/23006804/getting-error-error-cannot-find-module-express-after-npm-install
+
+Cannot find module 'socket.io' windows 10
+npm install --save socket.io
+The --save flag will add the given dependency to your application's package.json for easier installation in the future
